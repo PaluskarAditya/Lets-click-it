1eb0327e268948ac7ac1de05bead7347bc7e4dbf
Aid3n <adityapaluskar@gmail.com>
2026-03-13 23:27:03 +0530

    Added google adsense script

--- app/layout.jsx
+++ app/layout.jsx
@@ -55,6 +55,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6807284092712634"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
